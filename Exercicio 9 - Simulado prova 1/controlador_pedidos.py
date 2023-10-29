7c869a8e44daf36160c7ea2eefec216be0be8485
matheussteinbach <matheussteimbach4@gmail.com>
2023-10-29 00:52:20 -0300

    fix: arruma identacao e remove comentarios

--- Exercicio 9 - Simulado prova 1/controlador_pedidos.py
+++ Exercicio 9 - Simulado prova 1/controlador_pedidos.py
@@ -10,11 +10,6 @@ class ControladorPedidos():
     def pedidos(self):
         return self.__pedidos
 
-    '''
-    Busca pedido pelo numero.
-    Se o pedido nao existir, deve retornar None
-    Caso contrario, retorna o pedido.
-    '''
     def busca_pedido_por_numero(self, numero):
         if isinstance(numero, int):
             for pedido in self.pedidos:
@@ -22,12 +17,6 @@ class ControladorPedidos():
                     return pedido
             return None
 
-    '''
-    Incluir pedido na lista.
-    Tratar os casos de instancias incorretas e pedido duplicado.
-    Caso o pedido já exista na lista, gerar a excecao: 
-    PedidoDuplicadoException
-    '''
     def incluir_pedido(self, pedido):
         if isinstance(pedido, Pedido):
             numeros_pedidos = [
@@ -37,25 +26,13 @@ class ControladorPedidos():
             else:
                 raise PedidoDuplicadoException
 
-    '''
-    Exclui pedido pelo numero.
-    Se o pedido nao existir, deve retornar None
-    Caso contrario, retorna o pedido excluido
-    '''
     def excluir_pedido(self, numero):
         pedido = self.busca_pedido_por_numero(numero)
-        if pedido == None:
+        if not pedido:
             return None
         self.__pedidos.remove(pedido)
         return pedido
 
-    '''
-    Deve calcular o total do faturamento para todos os
-    itens pedidos por um CPF. 
-    Recebe como parametro:
-    distancia: um float que corresponde a distancia percorrida
-    cpf: uma string representando o CPF do Cliente a ser faturado
-    '''
     def calcular_faturamento_pedidos(self, distancia, cpf):
         faturamento = 0
         pedidos_por_cpf = []
@@ -64,4 +41,4 @@ class ControladorPedidos():
                 pedidos_por_cpf.append(pedido)
         for pedido in pedidos_por_cpf:
             faturamento += pedido.calcula_valor_pedido(distancia)
-            return faturamento
+        return faturamento
--- Exercicio 9 - Simulado prova 1/pedido.py
+++ Exercicio 9 - Simulado prova 1/pedido.py
@@ -39,12 +39,6 @@ class Pedido():
     def tipo(self, tipo):
         self.__tipo = tipo
 
-    '''
-    Inclui um novo item na lista de itens do pedido.
-    Nao deve ser possivel incluir itens duplicados (com o mesmo codigo).
-    Retornar o item incluido em caso de sucesso e None em caso
-    de item duplicado.
-    '''
     def inclui_item_pedido(self, codigo, descricao, preco):
         codigos = [item.codigo for item in self.itens]
         if codigo not in codigos:
@@ -53,10 +47,6 @@ class Pedido():
             return novo_item
         return None
 
-    '''
-    Exclui um item do pedido e retorna o item excluido
-    Caso o item nao exista, retorne None
-    '''
     def exclui_item_pedido(self, codigo):
         codigos = [item.codigo for item in self.itens]
         if codigo not in codigos:
@@ -66,20 +56,6 @@ class Pedido():
                 self.itens.remove(item)
                 return item
 
-    '''
-    Deve calcular o valor total do pedido, considerando um custo
-    adicional pela distancia e fator por distancia percorrida. 
-    O fator da distancia varia de acordo com o tipo de pedido.
-    O fator_distancia do TipoPedido deve ser multiplicado pela distancia 
-    e acrescido ao valor total dos itens. 
-    Por exemplo, se o fator_distancia for 2 e a distancia for 5, 
-    o total do pedido deve ser acrescido em 10. 
-    Ainda, se o cliente for ClienteFidelidade, deve  diminuir o valor total 
-    pelo percentual de desconto armazenado no atributo desconto do ClienteFidelidade. 
-    Por exemplo, se o valor de desconto for 0.2 e o pedido custar 10, o desconto deve ser
-    de 2 e o valor final 8.
-    @return um float correspondente ao total do pedido
-    '''
     def calcula_valor_pedido(self, distancia: float):
         valor_total = 0
         for item in self.itens:
@@ -89,8 +65,3 @@ class Pedido():
             desconto = valor_total * self.cliente.desconto
             valor_total -= desconto
         return valor_total
-
-
-pedido1 = Pedido(1, ClienteFidelidade(1, 0.2,'123','theus','rua','123'), TipoPedido('asdaf', 12))
-pedido1.inclui_item_pedido(1, 'Hamburguer', 12)
-pedido1.calcula_valor_pedido(5)
